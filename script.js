cee939834c65b8e8749bd091aebdf145ae7739e3
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-10-22 11:11:37 -0700

    variables are added

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// list of numbers
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// list of lowercase letters
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+// list of uppercase letters
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+//list of symbols
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+
+
+function generatePassword() {
+  //ask user for password length
+  const passwordLength = parseInt(promt("How many characters would you like in your password? Choose between 8 and 128"));
+  //ask if user wants number 
 
 
 
 
 
+}
 
 
 
